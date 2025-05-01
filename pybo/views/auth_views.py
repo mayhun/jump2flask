47137038516cd0c4cfdf17215f1f410e8be7c46e
may <mayhun28@gmail.com>
2025-05-01 13:40:48 +0900

    3-08 모델 수정하기(질문 답변 작성 시 사용자 정보 추가)

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -5,6 +5,7 @@ from werkzeug.utils import redirect
 from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
+import functools
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -37,7 +38,11 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user.id
-            return redirect(url_for('main.index'))
+            _next = request.args.get('next', '')
+            if _next:
+                return redirect(_next)
+            else:
+                return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login.html', form=form)
 
@@ -53,3 +58,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def warapped_view(*args, **kwargs):
+        if g.user is None:
+            _next = request.url if request.method == 'GET' else ''
+            return redirect(url_for('auth.login', next = _next))
+        return view(*args, **kwargs)
+    return warapped_view
